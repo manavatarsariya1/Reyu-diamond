819074039383881aacd285baadac22bffc5ad58d
manavatarsariya1 <manavtarsariya@logicgoinfotech.in>
2026-04-03 17:22:57 +0530

    feat: done

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
- baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+ baseURL: import.meta.env.VITE_API_URL || "https://reyu-diamond.onrender.com",
 //   withCredentials: true
 });
 
