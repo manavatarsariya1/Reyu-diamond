3f119e95de6eb0fbce41ddcf18fa88583f0794fb
manavatarsariya1 <manavtarsariya@logicgoinfotech.in>
2026-04-03 17:51:30 +0530

    feat: done

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
- baseURL: import.meta.env.VITE_API_URL || "https://reyu-diamond.onrender.com",
+ baseURL: import.meta.env.VITE_API_URL || "https://reyu-diamond.onrender.com/api",
 //   withCredentials: true
 });
 
