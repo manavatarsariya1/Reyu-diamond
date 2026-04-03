3a605210e3e27bc161a8a134629bbae3e74c432a
manavatarsariya1 <manavtarsariya@logicgoinfotech.in>
2026-04-03 12:37:31 +0530

    feat: fix

--- server/src/config/firebase-admin.config.ts
+++ server/src/config/firebase-admin.config.ts
@@ -1,25 +1,30 @@
 import admin from "firebase-admin";
-import path from "path";
-import serviceAccount from "../config/reyuDiamondKey.json" with { type: "json" };
 
 type Bucket = ReturnType<admin.storage.Storage["bucket"]>;
 
-const serviceAccountPath = path.resolve(
-  process.cwd(),
-  "config/reyuDiamondKey.json"
-);
+let serviceAccount;
+
+if (process.env.FIREBASE_CONFIG) {
+  // ✅ Production (Render)
+  serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+} else {
+  // ✅ Local fallback (optional)
+  serviceAccount = await import("../config/reyuDiamondKey.json", {
+    assert: { type: "json" }
+  });
+}
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(
-      serviceAccount as admin.ServiceAccount
-    ),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     storageBucket: "reyu-diamond-app.appspot.com",
   });
 }
 
 export const db = admin.firestore();
-export const fcm: admin.messaging.Messaging = admin.messaging();
+export const fcm = admin.messaging();
 export const bucket: Bucket = admin.storage().bucket();
 
 export default admin;
